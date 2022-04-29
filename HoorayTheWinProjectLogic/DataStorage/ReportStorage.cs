f32dba34138e10d662cccbc0b9b9c2a25343ffa4
Valeriya Puzikova <happyvaleriya@gmail.com>
2022-04-29 20:54:57 +0300

    Create methods for ReportStorage

--- HoorayTheWinProjectLogic/DataStorage/ReportStorage.cs
+++ HoorayTheWinProjectLogic/DataStorage/ReportStorage.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Json;
 
 namespace HoorayTheWinProjectLogic.DataStorage
 {
+    [Serializable]
     public class ReportStorage
     {
+        private const string filePath = @"hoorayProject.";
         public List<Report> Reports { get; private set; }
 
         private static ReportStorage _instance;
@@ -19,7 +22,26 @@ namespace HoorayTheWinProjectLogic.DataStorage
 
         public static ReportStorage GetInstance()
         {
+            if (_instance == null)
+            {
+                _instance = new ReportStorage();
+            }
             return _instance;
+        }
+
+        public string Serialize(List<Report> reports)
+        {
+            return JsonSerializer.Serialize<List<Report>>(reports);
+        }
+
+        public List<Report> Decerialize(string json)
+        {
+            if(json== null)
+            {
+                throw new ArgumentException("json");
+            }
+            return JsonSerializer.Deserialize<List<Report>>(json);
+
         }
 
         public static void SaveInstance()
