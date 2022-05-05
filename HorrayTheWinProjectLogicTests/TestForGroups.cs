8e74bbc05149c2e7a589571dc777f4acabcb7914
Ilya <Evmenya@gmail.com>
2022-05-05 16:37:19 +0300

    Create class to unittests

---
+++ HorrayTheWinProjectLogicTests/TestForGroups.cs
@@ -0,0 +1,16 @@
+using NUnit.Framework;
+
+namespace HorrayTheWinProjectLogicTests
+{
+    public class TestsForLinkList
+    {
+        //[TestCaseSource(typeof(AddInTheEndTestSource))]
+        //public void AddUserToGroup()
+        //{
+        //    list.AddInTheEnd(value);
+        //    LinkList actualList = list;
+        //    Assert.AreEqual(expectedList, actualList);
+        //}
+
+    }
+}
