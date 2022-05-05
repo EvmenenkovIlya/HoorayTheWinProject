b2259d30baf2e7baed5bb945cdf75e2ab60081f0
ViStrashko <voringir@mail.ru>
2022-05-05 23:08:33 +0300

    EnteringAResponseTests

---
+++ HorrayTheWinProjectLogicTests/EnteringAResponseTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Telegram.Bot.Types;
+using HoorayTheWinProjectLogic;
+using HoorayTheWinProjectLogic.Questions;
+
+
+
+namespace HorrayTheWinProjectLogicTests
+{
+    [TestFixture]
+    public class EnteringAResponseTests
+    {
+        [TestCase(-1.5, -2, 7)]
+        public void SetAnswerTest(Update update, Update expected)
+        {
+            double actual = HoorayTheWinProjectLogic.Questions.AbstractQuestion. (update);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
